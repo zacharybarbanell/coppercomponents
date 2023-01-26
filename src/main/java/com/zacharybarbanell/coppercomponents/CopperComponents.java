98c1363bc4c4a719502c424a4d0cbbb9b7a76e26
00gogo00 <zachgalaxy@comcast.net>
2023-01-26 13:18:02 -0800

    added redstone interaction to copper grate

--- src/main/java/com/zacharybarbanell/coppercomponents/CopperComponents.java
+++ src/main/java/com/zacharybarbanell/coppercomponents/CopperComponents.java
@@ -7,6 +7,7 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
@@ -25,26 +26,28 @@ import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(CopperComponents.MODID)
-public class CopperComponents
-{
+public class CopperComponents {
     // Define mod id in a common place for everything to reference
     public static final String MODID = "coppercomponents";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
-    // Create a Deferred Register to hold Blocks which will all be registered under the "coppercomponents" namespace
+    // Create a Deferred Register to hold Blocks which will all be registered under
+    // the "coppercomponents" namespace
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
-    // Create a Deferred Register to hold Items which will all be registered under the "coppercomponents" namespace
+    // Create a Deferred Register to hold Items which will all be registered under
+    // the "coppercomponents" namespace
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 
-    // Creates a new Block with the id "coppercomponents:example_block", combining the namespace and path
-    public static final RegistryObject<Block> EXAMPLE_BLOCK = 
-    BLOCKS.register("copper_grate", () -> new GrateBlock(BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_ORANGE).noOcclusion()));
-    // Creates a new BlockItem with the id "coppercomponents:example_block", combining the namespace and path
-    public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = 
-    ITEMS.register("copper_grate", () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_REDSTONE)));
+    // Creates a new Block with the id "coppercomponents:example_block", combining
+    // the namespace and path
+    public static final RegistryObject<Block> EXAMPLE_BLOCK = BLOCKS.register("copper_grate", () -> new GrateBlock(
+            BlockBehaviour.Properties.of(Material.METAL, MaterialColor.COLOR_ORANGE).isValidSpawn((a,b,c,d) -> {return false;}).noOcclusion().requiresCorrectToolForDrops().sound(SoundType.COPPER)));
+    // Creates a new BlockItem with the id "coppercomponents:example_block",
+    // combining the namespace and path
+    public static final RegistryObject<Item> EXAMPLE_BLOCK_ITEM = ITEMS.register("copper_grate",
+            () -> new BlockItem(EXAMPLE_BLOCK.get(), new Item.Properties().tab(CreativeModeTab.TAB_REDSTONE)));
 
-    public CopperComponents()
-    {
+    public CopperComponents() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         // Register the commonSetup method for modloading
@@ -59,8 +62,7 @@ public class CopperComponents
         MinecraftForge.EVENT_BUS.register(this);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event)
-    {
+    private void commonSetup(final FMLCommonSetupEvent event) {
         // Some common setup code
         LOGGER.info("HELLO FROM COMMON SETUP");
         LOGGER.info("DIRT BLOCK >> {}", ForgeRegistries.BLOCKS.getKey(Blocks.DIRT));
@@ -68,19 +70,17 @@ public class CopperComponents
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event)
-    {
+    public void onServerStarting(ServerStartingEvent event) {
         // Do something when the server starts
         LOGGER.info("HELLO from server starting");
     }
 
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    // You can use EventBusSubscriber to automatically register all static methods
+    // in the class annotated with @SubscribeEvent
     @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class ClientModEvents
-    {
+    public static class ClientModEvents {
         @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
+        public static void onClientSetup(FMLClientSetupEvent event) {
             // Some client setup code
             LOGGER.info("HELLO FROM CLIENT SETUP");
             LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
--- src/main/java/com/zacharybarbanell/coppercomponents/GrateBlock.java
+++ src/main/java/com/zacharybarbanell/coppercomponents/GrateBlock.java
@@ -13,6 +13,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SimpleWaterloggedBlock;
 import net.minecraft.world.level.block.state.BlockState;
@@ -21,6 +22,7 @@ import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.EntityCollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
@@ -28,13 +30,14 @@ import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class GrateBlock extends Block implements SimpleWaterloggedBlock {
    private static final Direction[] DIRECTIONS = Direction.values();
-   public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
    public static final BooleanProperty NORTH = BlockStateProperties.NORTH;
    public static final BooleanProperty EAST = BlockStateProperties.EAST;
    public static final BooleanProperty SOUTH = BlockStateProperties.SOUTH;
    public static final BooleanProperty WEST = BlockStateProperties.WEST;
    public static final BooleanProperty UP = BlockStateProperties.UP;
    public static final BooleanProperty DOWN = BlockStateProperties.DOWN;
+   public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
+   public static final BooleanProperty POWERED = BlockStateProperties.POWERED;
    public static final Map<Direction, BooleanProperty> PROPERTY_BY_DIRECTION = ImmutableMap
          .copyOf(Util.make(Maps.newEnumMap(Direction.class), (p_55164_) -> {
             p_55164_.put(Direction.NORTH, NORTH);
@@ -50,6 +53,11 @@ public class GrateBlock extends Block implements SimpleWaterloggedBlock {
    private static final VoxelShape EAST_AABB = Block.box(14.0D, 0.0D, 0.0D, 16.0D, 16.0D, 16.0D);
    private static final VoxelShape NORTH_AABB = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 16.0D, 2.0D);
    private static final VoxelShape SOUTH_AABB = Block.box(0.0D, 0.0D, 14.0D, 16.0D, 16.0D, 16.0D);
+   private static final VoxelShape CORE = Shapes.or(
+      Block.box(0.0D, 4.0D, 4.0D, 16.0D, 12.0D, 12.0D),
+      Block.box(4.0D, 0.0D, 4.0D, 12.0D, 16.0D, 12.0D),
+      Block.box(4.0D, 4.0D, 0.0D, 12.0D, 12.0D, 16.0D)
+   );
    public static final Map<Direction, VoxelShape> SHAPE_BY_DIRECTION = ImmutableMap
          .copyOf(Util.make(Maps.newEnumMap(Direction.class), (p_55164_) -> {
             p_55164_.put(Direction.NORTH, NORTH_AABB);
@@ -61,14 +69,14 @@ public class GrateBlock extends Block implements SimpleWaterloggedBlock {
          }));
    private final Map<BlockState, VoxelShape> shapesCache;
 
-   public GrateBlock(Properties p_49795_) {
-      super(p_49795_);
+   public GrateBlock(Properties properties) {
+      super(properties);
       this.registerDefaultState(
             this.stateDefinition.any()
                   .setValue(DOWN, Boolean.valueOf(false)).setValue(UP, Boolean.valueOf(false))
                   .setValue(NORTH, Boolean.valueOf(false)).setValue(EAST, Boolean.valueOf(false))
                   .setValue(SOUTH, Boolean.valueOf(false)).setValue(WEST, Boolean.valueOf(false))
-                  .setValue(WATERLOGGED, Boolean.valueOf(false)));
+                  .setValue(WATERLOGGED, Boolean.valueOf(false)).setValue(POWERED, Boolean.valueOf(false)));
       this.shapesCache = ImmutableMap.copyOf(this.stateDefinition.getPossibleStates().stream()
             .collect(Collectors.toMap(Function.identity(), GrateBlock::calculateShape)));
    }
@@ -80,6 +88,9 @@ public class GrateBlock extends Block implements SimpleWaterloggedBlock {
             voxelshape = Shapes.or(voxelshape, SHAPE_BY_DIRECTION.get(direction));
          }
       }
+      if (state.getValue(POWERED)) {
+         voxelshape = Shapes.join(voxelshape, CORE, BooleanOp.ONLY_FIRST);
+      }
       return voxelshape;
    }
 
@@ -88,6 +99,9 @@ public class GrateBlock extends Block implements SimpleWaterloggedBlock {
    }
 
    public VoxelShape getCollisionShape(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext ctx) {
+      if (state.getValue(POWERED)) {
+         return Shapes.empty();
+      }
       if (ctx instanceof EntityCollisionContext eCtx) {
          if (eCtx.getEntity() == null) {
             return this.getShape(state, getter, pos, ctx);
@@ -116,9 +130,9 @@ public class GrateBlock extends Block implements SimpleWaterloggedBlock {
          return blockstate.setValue(PROPERTY_BY_DIRECTION.get(ctx.getClickedFace()), true);
       } else {
          FluidState fluidstate = ctx.getLevel().getFluidState(ctx.getClickedPos());
-         boolean flag = fluidstate.getType() == Fluids.WATER;
-         return super.getStateForPlacement(ctx).setValue(WATERLOGGED, Boolean.valueOf(flag))
-               .setValue(PROPERTY_BY_DIRECTION.get(ctx.getClickedFace()), true);
+         return super.getStateForPlacement(ctx).setValue(WATERLOGGED, Boolean.valueOf(fluidstate.getType() == Fluids.WATER))
+               .setValue(PROPERTY_BY_DIRECTION.get(ctx.getClickedFace()), true)
+               .setValue(POWERED, Boolean.valueOf(ctx.getLevel().hasNeighborSignal(ctx.getClickedPos())));
       }
    }
 
@@ -129,11 +143,31 @@ public class GrateBlock extends Block implements SimpleWaterloggedBlock {
                   : super.canBeReplaced(state, ctx);
    }
 
-   protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> p_56120_) {
-      p_56120_.add(NORTH, EAST, SOUTH, WEST, UP, DOWN, WATERLOGGED);
+   protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+      builder.add(NORTH, EAST, SOUTH, WEST, UP, DOWN, WATERLOGGED, POWERED);
    }
 
-   public FluidState getFluidState(BlockState p_56969_) {
-      return p_56969_.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(p_56969_);
+   public FluidState getFluidState(BlockState state) {
+      return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+   }
+
+   public void neighborChanged(BlockState pState, Level pLevel, BlockPos pPos, Block pBlock, BlockPos pFromPos, boolean pIsMoving) {
+      if (!pLevel.isClientSide) {
+         boolean flag = pLevel.hasNeighborSignal(pPos);
+         if (flag != pState.getValue(POWERED)) {
+            if (pState.getValue(POWERED)) {
+               pLevel.levelEvent(null, 1037, pPos, 0);
+            }
+            else {
+               pLevel.levelEvent(null, 1037, pPos, 0);
+            }
+
+            pLevel.setBlock(pPos, pState.setValue(POWERED, Boolean.valueOf(flag)), 2);
+            if (pState.getValue(WATERLOGGED)) {
+               pLevel.scheduleTick(pPos, Fluids.WATER, Fluids.WATER.getTickDelay(pLevel));
+            }
+         }
+
+      }
    }
 }
